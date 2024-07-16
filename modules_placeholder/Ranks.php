b7d1487e1b801f8951ad0a252ed4d2252d267958
VennV <111500380+VennDev@users.noreply.github.com>
2024-07-16 23:20:42 +0700

    Update Ranks.php

--- modules_placeholder/Ranks.php
+++ modules_placeholder/Ranks.php
@@ -7,7 +7,8 @@ use _64FF00\PurePerms\PurePerms;
 use IvanCraft623\RankSystem\RankSystem;
 use IvanCraft623\RankSystem\utils\Utils;
 
-#Author Modules: ClickedTran | ClickedTran_VN
+$version = "1.0.0";
+$author = "ClickedTran(ClickedTran_VN)";
 
 #{pprank} = PurePerms
 #{rsrank} = Ranksystem
