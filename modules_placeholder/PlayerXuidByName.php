61479ab1351642237ec7ba39c3dd7d1b627279c7
VennV <111500380+VennDev@users.noreply.github.com>
2024-07-16 18:03:41 +0700

    Add files via upload

--- modules_placeholder/PlayerXuidByName.php
+++ modules_placeholder/PlayerXuidByName.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use pocketmine\Server;
 use venndev\vplaceholder\VPlaceHolder;
 
+$version = "1.0.0";
+$author = "VennDev";
+
 VPlaceHolder::registerPlaceHolder("{player_xuid_by_name}", function (string $player): string {
     $player = Server::getInstance()->getPlayerExact($player);
     if ($player === null) return "";
---
+++ modules_placeholder/VPlayerDataSaver.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use venndev\vapmdatabase\database\ResultQuery;
+use venndev\vplaceholder\VPlaceHolder;
+use venndev\vplayerdatasaver\VPlayerDataSaver;
+use vennv\vapm\Async;
+
+$version = "1.0.0";
+$author = "VennDev";
+
+/**
+ * This is a placeholder for VPlayerDataSaver plugin
+ * It contains some `async` methods from VPlayerDataSaver
+ */
+
+if (!class_exists(VPlayerDataSaver::class)) {
+    throw new RuntimeException("VPlayerDataSaver class not found! Install here: https://github.com/VennDev/VPlayerDataSaver");
+}
+
+if (!class_exists(Async::class)) {
+    throw new RuntimeException("Async class not found! Install here: https://github.com/VennDev/LibVapmPMMP");
+}
+
+if (!class_exists(ResultQuery::class)) {
+    throw new RuntimeException("ResultQuery class not found! Install here: https://github.com/VennDev/VapmDatabasePMMP");
+}
+
+VPlaceHolder::registerPlaceHolder("{vpds_get_name_player_by_xuid}", function (string $xuid): Async {
+    return new Async(function () use ($xuid) {
+        $data = Async::await(VPlayerDataSaver::getDataByXuid($xuid));
+        if ($data instanceof ResultQuery) $data = $data->getResult();
+        return $data["name"];
+    });
+}, true);
